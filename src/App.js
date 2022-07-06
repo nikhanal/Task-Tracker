d39f379f5ef00cceadbca5d27973cebf0e29c5a5
Nishan Khanal <nishankhanal@Spiny-Babler.local>
2022-07-06 22:31:26 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [tasks, setTask] = useState([
+    { id: 1, text: "Reminder 1", date: "Feb 3", reminder: true },
+    { id: 2, text: "Reminder 2", date: "Feb 4", reminder: false },
+    { id: 3, text: "Reminder 3", date: "Feb 5", reminder: true },
+  ]);
+
+  //add tasks
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1;
+    const newTask = { id, ...task };
+    setTask([...tasks, newTask]);
+  };
+
+  //delete props
+  const deleteTask = (id) => {
+    setTask(tasks.filter((task) => task.id !== id));
+  };
+  const toggleRemind = (id) => {
+    // console.log("helo");
+    setTask(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <AddTask onAdd={addTask} />
+
+      {tasks.length > 0 ? (
+        <Tasks task={tasks} onDelete={deleteTask} onToggle={toggleRemind} />
+      ) : (
+        "There are no tasks left"
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Tasks.js
@@ -0,0 +1,18 @@
+import Task from "./Task";
+
+const Tasks = ({ task, onDelete, onToggle }) => {
+  return (
+    <>
+      {task.map((task) => (
+        <Task
+          key={task.id}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
+      ))}
+    </>
+  );
+};
+
+export default Tasks;
